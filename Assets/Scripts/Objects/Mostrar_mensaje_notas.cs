896e1908735cbe867bc17e0da7ca89d0448d6eff
David Valderrama <david.valderrama@utp.edu.co>
2025-03-31 16:21:29 -0500

    Commit 4:21 p.m
    al acercarse a las notas se muestra un mensaje

---
+++ Assets/Scripts/Objects/Mostrar_mensaje_notas.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Mostrar_mensaje_notas : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI mensaje;
+    public GameObject Canva_del_mensaje;
+    private void Start()
+    {
+        if (mensaje != null)
+        {
+            mensaje.gameObject.SetActive(false); // Ocultamos el mensaje al inicio
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && mensaje != null)
+        {   Canva_del_mensaje.SetActive(true);
+            mensaje.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && mensaje != null)
+        {
+            mensaje.gameObject.SetActive(false);
+            Canva_del_mensaje.SetActive(false);
+        }
+    }
+}
